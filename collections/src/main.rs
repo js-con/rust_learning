3d0f53406f4fd17ea9c1a7cab886c72bb52c0ebf
lbw <574524020@qq.com>
2022-02-22 18:06:01 +0800

    collection: 60%

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -60,3 +60,57 @@ fn main() {
 
     //如果不知道运行时Vec里的数据类型,就不能用枚举了,就需要用trait,见17章
 }
+
+fn learning_string(){
+    // String和slice都是UTF-8编码的
+    // 标准库里有其他编码或者不同内存表现形式的字符串，如：OsString, OsStr, CString, CStr等
+
+    {
+        //初始化字符串
+        let data = String::from("initial contents");
+
+        let data = "initial contents";
+
+        let s = data.to_string(); //to_string可用于任何被实现了Display trait的类型
+
+        let s = "initial contents".to_string(); //字符串字面值也实现了该trait
+    }
+    {
+        //更新字符串
+        let mut s = String::from("foo");
+        let s2 = "bar";
+        s.push_str(s2); 
+        println!("s2 is {}",s2);//push_str使用的是slice,所以不需要交出所有权
+
+        s.push('l'); //push方法只能传一个字符
+
+        let s1 = String::from("Hello, ");
+        let s2 = String::from("world!");
+        let s3 = s1 + &s2; //注意 s1 被移动了，不能继续使用
+        
+        /* 
+        之所以s2需要借用,因为+运算符这里使用了add函数,它的签名大概是这样的
+            fn add(self, s:&str) -> String {}
+        但是我们传的是&String 而不是 &s2,却仍然能通过编译
+        因为&String可以被强转(deref coerced)成&str,编译器把&s2强转成了&s2[..]
+        又因为签名中是self而不是&self，所以这里的s1所有权移动到了add里 
+        */
+
+        //使用format!拼接复杂字符串
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+
+        let s = format!("{}-{}-{}",s1,s2,s3); //format!使用slice,所以不会夺取所有权
+    }
+    
+    {
+        //Rust的字符串不支持索引
+        let s1 = String::from("hello");
+        //let h = s1[0]; error
+
+        /* 
+            关于为什么rust字符串不支持索引
+        */
+    }
+}
