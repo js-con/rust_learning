3cfdfcaa558fd58d6640d5c44d702d2b1f394060
Walker <ss.lbw.ss@gmail.com>
2022-07-29 18:48:57 +0800

    🦄 refactor: code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // args[0] 保存的是程序名
-    let query = &args[1];
-    let filename = &args[2];
-    
-    println!("Searching for {},in file {}", query, filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Error when reading the file!");
-    println!("With test:\n {}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
 }
