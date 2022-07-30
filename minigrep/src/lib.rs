5d2859150d35009b57f262a9947097be70b4fdec
Walker <ss.lbw.ss@gmail.com>
2022-07-30 18:04:09 +0800

    🐞 fix: fix search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,12 +23,21 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    let line = contents
-        .lines()
-        .filter(|&line| line.contains(query))
-        .next()
-        .unwrap();
-    results.push(line);
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            results.push(line)
+        }
+    }
     results
 }
 
@@ -42,7 +51,22 @@ mod tests {
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
     }
 }
