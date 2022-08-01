3808205cda68012a8d6aa867a52a68da07a5a527
Walker <ss.lbw.ss@gmail.com>
2022-08-01 11:11:30 +0800

    ✨ feat: refactor search func

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -48,13 +48,10 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
