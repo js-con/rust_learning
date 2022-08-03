4db2016d176bc1f9518157012aded1e01d727b6f
Walker <ss.lbw.ss@gmail.com>
2022-08-03 14:19:17 +0800

    ✨ feat(list): A Bad Stack

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,11 +1,81 @@
-pub struct List{
-  head: Link
+use std::{fmt::Display, mem};
+pub struct List {
+    head: Link,
 }
 enum Link {
-   Empty,
-   More(Box<Node>),
+    Empty,
+    More(Box<Node>),
 }
-struct Node{
-  elem: i32,
-  next: Link
+impl Display for Link {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Link::Empty => write!(f, "{}", "nil"),
+            Link::More(node) => write!(f, "{}", node),
+        }
+    }
+}
+struct Node {
+    elem: i32,
+    next: Link,
+}
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{},{}", self.elem, self.next)
+    }
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None ,
+            Link::More(node) => {
+              self.head = node.next;
+              Some(node.elem)
+            }
+        }
+    }
+}
+impl Drop for List {
+    fn drop(&mut self) {
+      let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+      while let Link::More(mut boxed_node) = cur_link {
+        cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+      }  
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn basics() {
+      let mut list = List::new();
+      assert_eq!(list.pop(), None);
+
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      assert_eq!(list.pop(),Some(3));
+      assert_eq!(list.pop(),Some(2));
+
+      list.push(4);
+      list.push(5);
+
+      assert_eq!(list.pop(),Some(5));
+      assert_eq!(list.pop(),Some(4));
+      assert_eq!(list.pop(),Some(1));
+      assert_eq!(list.pop(),None);
+    }
 }
