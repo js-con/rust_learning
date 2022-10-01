2877ad09a97c5adb7131c7ee14d431fc1fa368ad
004725 <ss.lbw.ss@gmail.com>
2022-10-01 20:39:38 +0800

    ✨ feat(web-server): base response

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::Read,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -16,5 +16,9 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 ok\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
