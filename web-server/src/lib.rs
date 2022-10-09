2d1e474e6f20a3f0317176be9c613e54b58a0c43
004725 <ss.lbw.ss@gmail.com>
2022-10-09 19:03:22 +0800

    ✨ feat(web-server): workers,channel

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,16 +1,40 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
+struct Job;
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
+    }
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {}
 
-        ThreadPool { threads }
+        let mut workers = Vec::with_capacity(size);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
