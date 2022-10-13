75e6a55007ec92b9335ef9ae793da3c2f9816a87
004725 <ss.lbw.ss@gmail.com>
2022-10-13 19:19:41 +0800

    ✨ feat(concurrency): mpsc channel

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,7 +1,12 @@
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, sync::mpsc};
 
 fn main() {
     let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || println!("Here's a vector: {:?}", v));
-    handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = "hi".to_string();
+        tx.send(val).unwrap();
+    });
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
